90db10b37c0b84fd20a455ffac2cd6ad47be1747
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-10-10 13:39:25 -0600

    Update helpers.py
    Add in helper functions from https://github.com/SynBioDex/SBOL-examples/blob/site-variant-libraries/SBOL/curated-examples/site-variant-libraries/excel_helpers.py

--- excelutils/excel_sbol_utils/helpers.py
+++ excelutils/excel_sbol_utils/helpers.py
@@ -1,5 +1,7 @@
 import re
 import string
+import openpyxl as opxl
+from pathlib import Path
 
 def check_name(nm_to_chck):
     """the function verifies that the names is alphanumeric and
@@ -70,7 +72,7 @@ def truthy_strings(to_check):
 
 def col_to_num(col_name):
     """takes an excel column name, e.g. AA and converts it to a
-    zero indexed number e.g. 26
+    one indexed number e.g. 27
 
     Args:
        col_name (string): An excel formatted column name, e.g. AA
@@ -81,7 +83,7 @@ def col_to_num(col_name):
                     or contains spaces
 
     Returns:
-        num (integer): A zero indexed column index
+        num (integer): A one indexed column index
     """
 
     if type(col_name) != str:
@@ -93,10 +95,54 @@ def col_to_num(col_name):
     elif len(col_name) > 3:
         # too long to be an excel column name
         raise ValueError
+    return opxyl.worksheet.cell_range.range_boundaries(f'{column_letter}1')[0]
 
-    num = 0
-    for ltr in col_name:
-        if ltr in string.ascii_letters:
-            num = num * 26 + (ord(ltr.upper()) - ord('A')) + 1
-    num = num - 1
-    return (num)
+def row_ends(sheet: worksheet, row: int, min_col: int = 1) -> int:
+    """Find the column at which a row ends
+    :param sheet: Sheet to search
+    :param row: Row to search
+    :param min_col: Column on which to start, defaults to first column
+    :return: numerical value of last column containing data
+    """
+    # convert min column to numerical to determine starting point
+    column_iterator = sheet.iter_cols(min_row=row, max_row=row, min_col=min_col, values_only=True)
+    max_sequence_length = 100000  # TODO: use a proper generator counter rather than a big range
+    return next(i-1 for v, i in zip(column_iterator, range(min_col, max_sequence_length)) if not v[0])
+
+
+def read_variant_table(excel_file: Path) -> tuple[str, str, list[list]]:
+    """Extract an amino acid site-variant table from a Twist amino-acid site-variant library Excel sheet
+    :param excel_file: location of file to read
+    :return: Tuple of library name, original sequence, list of variant lists for each site, in order
+    """
+    VARIANTS_SHEET = 'Amino Acid Variants'
+    LIBRARY_NAME_CELL = 'C10'
+    FIRST_AMINO_ACID_COLUMN = 'F'
+    ORIGINAL_AMINO_ACID_ROW = 12
+    FIRST_VARIANT_ROW = 14
+    LAST_VARIANT_ROW = 35
+    
+    print(f'Loading workbook "{excel_file}"')
+    work_book = openpyxl.load_workbook(excel_file, data_only=True)
+    sheet = work_book[VARIANTS_SHEET]
+
+    # First, get the library name
+    library_name = sheet[LIBRARY_NAME_CELL].value
+    print(f'Library is named "{library_name}"')
+
+    # Then get the base sequence
+    print('Extracting base sequence')
+    first_aa_column = get_column_number(FIRST_AMINO_ACID_COLUMN)
+    last_aa_column = row_ends(sheet, ORIGINAL_AMINO_ACID_ROW, first_aa_column)
+    # Get row from sheet and concatenate it into a string
+    row_iterator = sheet.iter_rows(min_row=ORIGINAL_AMINO_ACID_ROW, max_row=ORIGINAL_AMINO_ACID_ROW,
+                                   min_col=first_aa_column, max_col=last_aa_column, values_only=True)
+    base_sequence = [''.join(row) for row in row_iterator][0]
+    print(f'Found sequence {len(base_sequence)} residues long: "{base_sequence}"')
+
+    # Finally, get all the variant lists
+    column_iterator = sheet.iter_cols(min_row=FIRST_VARIANT_ROW, max_row=LAST_VARIANT_ROW,
+                                      min_col=first_aa_column, max_col=last_aa_column, values_only=True)
+    variant_lists = [[v for v in column if v] for column in column_iterator]  # drop the empty cells from each range
+
+    return library_name, base_sequence, variant_lists
